6893b1370911b220a350ec516b652338f593b606
dhruvre <dhruvrathore3002@gmail.com>
2025-08-12 00:03:04 +0530

    app add

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({"status": "UP"}), 200
+
+@app.route("/user", methods=["GET"])
+def get_user():
+    return jsonify({
+        "id": 1,
+        "name": "Dhruv",
+        "role": "Developer"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
